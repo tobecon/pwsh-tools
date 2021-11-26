48040555bfd6302c45cfc98184d9753458732c0f
chenchu <chenchu@netsdl.com>
2021-11-26 15:30:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# pwsh-tools
+
+## 这里有一些小函数, 用来管理你电脑上的项目, 
+
+
+### 开始之前, 你需要设置一些环境变量
+
+- 1. 将 VisualStudio 放在环境变量的 Path 中,比如我的vs 安装路径, C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE
+
+### 一些命令说明
+
+- pcd , 用来切换到项目文件夹
+- pvs , 用来打开文件夹下的解决方案
+- pcmd , 用来执行确定的解决方案
+- gitvv , 用来执行git remote -v 命令
---
+++ profile.ps1
@@ -0,0 +1,62 @@
+$projlist = "~/.projlist"
+Set-Alias vs devenv.exe
+
+
+function gremote {
+    git remote -v
+}
+
+function lsgrep(# Parameter help description
+    [string]
+    $content) {
+    Get-ChildItem "." | Where-Object { $_.Name -like $content }
+}
+
+function vsopen {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $Dir,
+        [Parameter()]
+        [string]
+        $RequireAdmin
+    )
+    $proj = Get-ChildItem $Dir | Where-Object { $_.Name -like "*sln" }
+    if ($RequireAdmin -eq "true") {
+        Start-Process devenv.exe -Verb RunAs -ArgumentList $proj
+    }
+    else {
+        devenv.exe $proj
+    }
+}
+function FindProject ([string] $proj) {
+    $p = Import-Csv -Path $projlist -Delimiter "," | Where-Object { $_.Name -like $proj }
+    $project = [PSCustomObject]@{
+        Name         = $p.Name
+        Path         = $p.Path
+        RequireAdmin = $p.RequireAdmin
+    }
+    $project
+}
+
+function pcd ([string] $proj) {
+    $p = FindProject($proj)
+    Set-Location  $p.Path
+}
+
+function pvs ([string] $proj) {
+    $p = FindProject($proj)
+    $p
+    vsopen -Dir $p.Path -RequireAdmin $p.RequireAdmin
+}
+
+function pcmd ([string] $proj) {
+    $p = FindProject($proj)
+    $p
+    pwsh.exe $p.Path
+}
+function git_remtoe_v {
+    git remote -v
+}
+Set-Alias gitvv git_remtoe_v
